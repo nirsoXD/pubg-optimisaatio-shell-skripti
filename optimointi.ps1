7281a2c4dd4fcf6c65eb33977ccd4d1113e14c7f
Verneri Vesterinen <rikottu314@gmail.com>
2023-05-18 12:50:39 +0300

    Initial commit

---
+++ optimointi.ps1
@@ -0,0 +1,76 @@
+$programName = "PUBG: BATTLEGROUNDS"
+
+#Etsitään polku pubgi asennukseen
+$program = Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" |
+           Where-Object { $_.DisplayName -eq $programName } |
+           Select-Object -First 1
+
+if ($program) {
+    $installationLocation = $program.InstallLocation
+    Write-Host "Program Name: $($program.DisplayName)"
+    Write-Host "Installation Location: $installationLocation"
+} else {
+    Write-Host "Pubgi ei asennettu koneelle"
+	Pause
+	Exit
+	
+}
+
+#Poistetaan leffat
+$movieLocation = "\TslGame\Content\Movies"
+$folderPath = "{0}{1}" -f $installationLocation, $movieLocation
+Write-Host "Poistetaan leffat sijainnista: $folderPath"
+
+Get-ChildItem -Path $folderPath -File -Recurse | Remove-Item -Force
+
+$folders = Get-ChildItem -Path $folderPath -Directory -Recurse | Select-Object -ExpandProperty FullName
+
+foreach ($folder in $folders) {
+    Get-ChildItem -Path $folder -File | Remove-Item -Force
+}
+
+
+
+#Gameusersettings hommat
+#Etsitään appdata polku TSLGAME kansioon
+$folderName = "TslGame"
+$appDataFolderPath = Join-Path $env:LOCALAPPDATA $folderName
+Write-Host "Path to $folderName folder in AppData: $appDataFolderPath"
+
+#Lisätään polkuun gameusersettings.ini tiedoston sijainti turvaan kopiointia varten
+$gameusersettingslisapolku = "\Saved\Config\WindowsNoEditor\GameUserSettings.ini"
+
+#Yhdistetään polut ja kopioidaan tiedosto turvaan
+$sourceFilePath = "{0}{1}" -f $appDataFolderPath, $gameusersettingslisapolku
+Copy-Item -Path $sourceFilePath -Destination $appDataFolderPath
+
+$appDataPoistettavat = "{0}{1}" -f $appDataFolderPath, "\Saved"
+
+#Deletoidaan kansion sisältö
+Get-ChildItem -Path $appDataPoistettavat -File -Recurse | Remove-Item -Force
+
+$poistettavatKansiot = Get-ChildItem -Path $appDataPoistettavat -Directory -Recurse | Select-Object -ExpandProperty FullName
+
+foreach ($kansio in $poistettavatKansiot) {
+    Get-ChildItem -Path $kansio -File | Remove-Item -Force
+}
+
+#Kopioidaan gameusersettings.ini takaisin paikoilleen
+$gameusersettingsalkup = "{0}{1}" -f $appDataFolderPath, "\Saved\Config\WindowsNoEditor\"
+$gameusersettingsbackup = "{0}{1}" -f $appDataFolderPath, "\GameUserSettings.ini"
+Copy-Item -Path $gameusersettingsbackup -Destination $gameusersettingsalkup
+Pause
+
+$keywords = "ScreenScale"
+$keywords = "ScreenScale", "InGameFrameRateLimitType", "InGameCustomFrameRateLimit",`
+"MasterSoundVolume", "EffectSoundVolume", "EmoteSoundVolume", "UISoundVolume",`
+"BGMSoundVolume", "PlaygroundBGMSoundVolume",`
+"PlaygroundWebSoundVolume", "FpsCameraFov", "Gamma" 
+$excludedKeyword = "TslPersistant"
+
+Get-Content -Path $gameusersettingsbackup |
+    Select-String -Pattern $keywords |
+    Where-Object { $_ -notmatch $excludedKeyword }
+
+
+Pause
